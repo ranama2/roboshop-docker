c4d8695be0a7dfdc86c4aa1fba9b67af2fee3ce6
Meherun Mithila <mnm19322@email.vccs.edu>
2025-05-28 02:39:51 -0400

    set all configs

--- main.tf
+++ main.tf
@@ -7,5 +7,6 @@ module "ec2" {
   instance_type           = each.value["instance_type"]
   name                    = each.key
   zone_id                 = var.zone_id
+  vpc_security_group_ids  = var.vpc_security_group_ids
   //ansible_role            = lookup(each.value, "ansible_role", each_key)
 }
